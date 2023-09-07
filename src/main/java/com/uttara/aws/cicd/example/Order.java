faba9704104050888db25937b82e2db231b40ed3
sobha13 <sobhapithavath1301@gmail.com>
2023-09-07 18:19:14 +0530

    first commit to my repo.

--- src/main/java/com/uttara/aws/cicd/example/Order.java
+++ src/main/java/com/uttara/aws/cicd/example/Order.java
@@ -1,5 +1,7 @@
 package com.uttara.aws.cicd.example;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,12 +11,22 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class Order {
-    private int id;
+public class Order implements Serializable {
+ 
+	public Order(int i, String string, int j, int k) {
+		// TODO Auto-generated constructor stub
+	}
+	private int id;
     private String name;
     private int quantity;
-
     private long price;
-
+	/*
+	 * public int getId() { return id; } public void setId(int id) { this.id = id; }
+	 * public String getName() { return name; } public void setName(String name) {
+	 * this.name = name; } public int getQuantity() { return quantity; } public void
+	 * setQuantity(int quantity) { this.quantity = quantity; } public long
+	 * getPrice() { return price; } public void setPrice(long price) { this.price =
+	 * price; }
+	 */
 
 }
--- src/main/java/com/uttara/aws/cicd/example/OrderServiceApplication.java
+++ src/main/java/com/uttara/aws/cicd/example/OrderServiceApplication.java
@@ -1,5 +1,7 @@
 package com.uttara.aws.cicd.example;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,26 +9,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @SpringBootApplication
 @RestController
 @RequestMapping("/orders")
 public class OrderServiceApplication {
 
-    @Autowired
-    private OrderDao orderDao;
+	@Autowired
+	private OrderDao orderDao;
 
-    @GetMapping
-
+	@GetMapping
     public List<Order> fetchOrders() {
-        return orderDao.getOrders().stream().sorted(Comparator.comparing(Order::getPrice)).collect(Collectors.toList());
+
+		/*
+		 * return
+		 * orderDao.getOrders().stream().sorted(Comparator.comparing(Order::getPrice()))
+		 * .collect(Collectors.toList());
+		 */
+		return orderDao.getOrders();
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(OrderServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
 
 }
