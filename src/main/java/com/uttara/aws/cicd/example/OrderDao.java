8e5f44dc751c7e67cee49fc017bb2e36725deb2f
sobha <sobhapithavath1301@gmail.com>
2023-09-13 23:58:25 +0530

    Dao commit.

--- src/main/java/com/uttara/aws/cicd/example/OrderDao.java
+++ src/main/java/com/uttara/aws/cicd/example/OrderDao.java
@@ -14,7 +14,8 @@ public class OrderDao {
         return Stream.of(new Order(101, "Mobile", 1, 30000),
                 new Order(58, "Book", 4, 2000),
                 new Order(205, "Laptop", 1, 150000),
-                new Order(205, "Tab", 1, 150000),
+                new Order(204, "Tab", 1, 150000),
+                new Order(206, "TV", 1, 200000),
                 new Order(809, "headset", 1, 1799)).collect(Collectors.toList());
     }
 }
